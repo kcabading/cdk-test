b5c803fb2cf4ac5f4acf359eb417cf3958310bbe
EAD\kcabading2 <kcabading2@dxc.com>
2025-01-15 22:52:25 +0800

    Added buildspec.yml file

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ GalaxyCdk2Stack(app, "GalaxyCdk2Stack",
     # Uncomment the next line if you know exactly what Account and Region you
     # want to deploy the stack to. */
 
-    #env=cdk.Environment(account='123456789012', region='us-east-1'),
+    env=cdk.Environment(account='193365704239', region='us-east-1'),
 
     # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
     )
