028d2ea0ac2b1c4db3c505b05fbe4508bf87100b
EAD\kcabading2 <kcabading2@dxc.com>
2025-01-17 14:00:10 +0800

    Updated stack to use pipeline

--- tests/unit/test_resource_stack.py
+++ tests/unit/test_resource_stack.py
@@ -1,7 +1,7 @@
 import aws_cdk as core
 import aws_cdk.assertions as assertions
 
-from galaxy_cdk2.galaxy_cdk2_stack import GalaxyCdk2Stack
+from resource_stack.resource_stack import GalaxyCdk2Stack
 
 # example tests. To run these tests, uncomment this file along with the example
 # resource in galaxy_cdk2/galaxy_cdk2_stack.py
