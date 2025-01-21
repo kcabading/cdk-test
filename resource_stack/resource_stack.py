ec97512f0531e691fe31f810c8446fcf45477927
EAD\kcabading2 <kcabading2@dxc.com>
2025-01-21 15:44:15 +0800

    Added api in the stack

--- resource_stack/resource_stack.py
+++ resource_stack/resource_stack.py
@@ -3,6 +3,7 @@ from aws_cdk import (
     aws_sqs as sqs,
     Duration,
     aws_lambda as function_lambda,
+    aws_apigateway as apigateway,
     aws_s3 as s3
 )
 from constructs import Construct
@@ -19,12 +20,27 @@ class ResourceStack(Stack):
             queue_name="galaxy_queue"
         )
 
-        function = function_lambda.Function(self,
+
+
+        account_inventory_function = function_lambda.Function(self,
                                             "DemoCDKGITHUBLambda",
                                             function_name="codepipeline_lambda",
                                             runtime=function_lambda.Runtime.PYTHON_3_9,
                                             code=function_lambda.Code.from_asset('./lambda'),
                                             handler="account_inventory.lambda_handler")
+        
+        # Define the API Gateway resource
+        api = apigateway.LambdaRestApi(
+            self,
+            "HelloWorldApi",
+            handler = account_inventory_function,
+            proxy = False,
+        )
+        
+        # Define the '/hello' resource with a GET method
+        hello_resource = api.root.add_resource("hello")
+        hello_resource.add_method("GET")
+
 
         bucket = s3.Bucket(self, "MyfirstBucket", versioned=True,
                            bucket_name="account-inventory-bucket-123456",
