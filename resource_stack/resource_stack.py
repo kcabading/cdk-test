a1c5d90dadcb0713904fb311e71c68aac6170922
EAD\kcabading2 <kcabading2@dxc.com>
2025-01-22 18:55:55 +0800

    Updated API gateway

--- resource_stack/resource_stack.py
+++ resource_stack/resource_stack.py
@@ -27,20 +27,20 @@ class ResourceStack(Stack):
                                             runtime=function_lambda.Runtime.PYTHON_3_9,
                                             code=function_lambda.Code.from_asset('./lambda'),
                                             handler="account_inventory.lambda_handler",
-                                            timeout=Duration.seconds(5)),
+                                            timeout=Duration.seconds(5))
         
         #Define the API Gateway resource
-        # api = apigateway.LambdaRestApi(
-        #     self,
-        #     "HelloWorldApi",
-        #     handler = account_inventory_function
-        # )
+        api = apigateway.LambdaRestApi(
+            self,
+            "HelloWorldApi",
+            handler = account_inventory_function
+        )
         
-        # # Define the '/hello' resource with a GET method
+        # Define the '/hello' resource with a GET method
         # hello_resource = api.root.add_resource("hello")
         # hello_resource.add_method("GET")
 
 
-        # bucket = _s3.Bucket(self, "MyfirstBucket", versioned=True,
-        #                    bucket_name="account-inventory-bucket-123456",
-        #                    block_public_access=_s3.BlockPublicAccess.BLOCK_ALL)
+        bucket = _s3.Bucket(self, "MyfirstBucket", versioned=True,
+                           bucket_name="account-inventory-bucket-123456",
+                           block_public_access=_s3.BlockPublicAccess.BLOCK_ALL)
